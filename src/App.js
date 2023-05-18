93e74640d5824e09cea0810cbe1c40616cd34a9f
Elena-Polyakova <72936523+Elena-Polyakova@users.noreply.github.com>
2023-05-18 18:54:18 -0400

    Non serializable value middleware used.

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
-import { setCurrentUser } from './store/user/user.action';
+import { setCurrentUser } from './store/user/user.reducer';
 
 const App = () => {
   const dispatch = useDispatch();
@@ -22,7 +22,9 @@ const App = () => {
       if (user) {
         createUserDocumentFromAuth(user);
       }
-      dispatch(setCurrentUser(user));
+      //if user authenticated, select values wanted from auth and put them in new object
+      const pickedUser = user && (({accessToken, email }) => ({ accessToken, email }))(user);
+      dispatch(setCurrentUser(pickedUser));
     });
 
     return unsubscribe;
